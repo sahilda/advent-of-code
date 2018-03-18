b0c91c2b8a83c5a674da593b5512a956f2c108c7
Sahil Agarwal <sahildagarwal@gmail.com>
2018-03-17 23:16:34 -0700

    Completed day 25

--- solutions/day25.rb
+++ solutions/day25.rb
@@ -64,7 +64,68 @@ class Run
     end
 end
 
+class RunA < Run
+
+    def do_move
+        if @tape[@cursor] == 0
+            @tape[@cursor] = 1
+            case @state
+            when :a
+                go_right
+                @state = :b            
+            when :b
+                go_left
+                @state = :a
+            when :c
+                go_right
+                @state = :a
+            when :d
+                go_right
+                @state = :a
+            when :e
+                go_left
+                @state = :f
+            when :f
+                go_right
+                @state = :d
+            end
+        else
+            case @state
+            when :a
+                @tape[@cursor] = 0
+                go_left
+                @state = :c
+            when :b
+                @tape[@cursor] = 1
+                go_right
+                @state = :d
+            when :c
+                @tape[@cursor] = 0
+                go_left
+                @state = :e
+            when :d
+                @tape[@cursor] = 0
+                go_right
+                @state = :b
+            when :e
+                @tape[@cursor] = 1
+                go_left
+                @state = :c
+            when :f
+                @tape[@cursor] = 1
+                go_right
+                @state = :a
+            end
+        end
+    end
+
+end
+
 p1 = Run.new
 p1.move(6)
 p p1.tape
 p p1.get_checksum
+
+p2 = RunA.new
+p2.move(12919244)
+p p2.get_checksum
